9066cfee4caa8fab173aba999f9ba5f26a129761
rodrigogielow <rodrigo.gielow@southsystem.com.br>
2022-01-12 09:34:42 -0300

    Apply api mock pattern.

---
+++ events/src/main/java/br/com/giovannicampos/events/framework/models/PersonModel.kt
@@ -0,0 +1,21 @@
+package br.com.giovannicampos.events.framework.models
+
+import com.google.gson.annotations.SerializedName
+
+data class PersonModel(
+    @SerializedName("id") val id: String = "",
+    @SerializedName("eventId") val eventId: String = "",
+    @SerializedName("name") val name: String = "",
+    @SerializedName("email") val email: String = "",
+    @SerializedName("picture") val picture: String = ""
+){
+    companion object{
+        fun mock() = PersonModel(
+            id = "21",
+            eventId = "32",
+            name = "Rodrigo Gielow",
+            email = "rodrigo@abc.com.br",
+            picture = ""
+        )
+    }
+}
--- events/src/main/java/br/com/giovannicampos/events/mappers/PersonMapper.kt
+++ events/src/main/java/br/com/giovannicampos/events/mappers/PersonMapper.kt
@@ -2,10 +2,11 @@ package br.com.giovannicampos.events.mappers
 
 import br.com.giovannicampos.events.domain.Person
 import br.com.giovannicampos.events.framework.models.EventModel
+import br.com.giovannicampos.events.framework.models.PersonModel
 
 object PersonMapper {
 
-    fun toModel(entity: Person) = EventModel.PersonModel(
+    fun toModel(entity: Person) = PersonModel(
         entity.id,
         entity.eventId,
         entity.name,
@@ -13,7 +14,7 @@ object PersonMapper {
         entity.picture
     )
 
-    fun toEntity(model: EventModel.PersonModel) = Person(
+    fun toEntity(model: PersonModel) = Person(
         model.id,
         model.eventId,
         model.name,
